5849fd1d97eaae5442a107872bbfda3005d74501
Rui Salgado <ruivfsalgado@gmail.com>
2015-06-11 22:24:49 +0100

    Chapter 3 exercises

--- chapter3/src/Data/PhoneBook.purs
+++ chapter3/src/Data/PhoneBook.purs
@@ -23,3 +23,23 @@ findEntry firstName lastName = head <<< filter filterEntry
   where
   filterEntry :: Entry -> Boolean
   filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
+
+hasName :: String -> String -> Entry -> Boolean
+hasName firstName lastName entry = entry.firstName == firstName && entry.lastName == lastName
+
+findByName :: String -> String -> PhoneBook -> Maybe Entry
+findByName firstName lastName = findEntry (hasName firstName lastName)
+
+findByPhone :: String -> PhoneBook -> Maybe Entry
+findByPhone phoneNumber = findEntry filterEntry
+  where
+  filterEntry entry = entry.phone == phoneNumber
+
+hasEntryWithName :: String -> String -> PhoneBook -> Boolean
+hasEntryWithName firstName lastName book = not $ null $ filter (hasName firstName lastName) book
+
+removeDuplicates :: String -> String -> PhoneBook -> PhoneBook
+removeDuplicates firstName lastName = nubBy sameName
+  where
+  sameName :: Entry -> Entry -> Boolean
+  sameName one other = one.firstName == other.firstName && one.lastName == other.lastName
